cdc3f1b0af7b32a80535b16301982591b15070a1
Joe Filippazzo <joe@adrianholovaty.journalism.cuny.edu>
2010-12-02 16:39:27 +0000

    Srtipped the categories page down to the bones as per Almudena's request and modified the CSS a bit. Created the slideshow page template that drops the digitalstoragespace slideshow in as an iframe with a custom field.

---
+++ page-slideshow.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Template Name: Slideshow Page
+*/
+?>
+
+<?php get_header(); ?>
+<!-- <div class="span-<?php if (get_option('gpp_sidebar') == 'true' || get_option('gpp_sidebar') === FALSE) { echo "15 colborder home"; } else { echo "24 last"; } ?>"> -->
+<div class="share-yours">
+	<h2><?php the_title(); ?></h2>
+	<div class="content">
+		<iframe src="<?php if( $slideshow_url = get_post_meta($post->ID, 'slideshow_url', true) ) { echo $slideshow_url; } ?>" width="950" height="600" scrolling="no"></iframe>
+	</div>
+<?php get_footer(); ?>
